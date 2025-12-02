69f5b1b55e8a783bf6ff8285e4c24e772c06407d
dowon <dev@yumyum.im>
2025-12-02 16:26:34 +0900

    Hero 섹션 아이콘 제거, 텍스트만 유지

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,27 +20,12 @@ function Hero() {
             </p>
             <div className="hero-values">
               <div className="value-item">
-                <div className="value-icon">
-                  <svg width="48" height="48" viewBox="0 0 24 24" fill="none">
-                    <path d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z" fill="currentColor"/>
-                  </svg>
-                </div>
                 <div className="value-text">혁신</div>
               </div>
               <div className="value-item">
-                <div className="value-icon">
-                  <svg width="48" height="48" viewBox="0 0 24 24" fill="none">
-                    <path d="M12 21.35L10.55 20.03C5.4 15.36 2 12.28 2 8.5C2 5.42 4.42 3 7.5 3C9.24 3 10.91 3.81 12 5.09C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.42 22 8.5C22 12.28 18.6 15.36 13.45 20.04L12 21.35Z" fill="currentColor"/>
-                  </svg>
-                </div>
                 <div className="value-text">신뢰</div>
               </div>
               <div className="value-item">
-                <div className="value-icon">
-                  <svg width="48" height="48" viewBox="0 0 24 24" fill="none">
-                    <path d="M16 6L18.29 8.29L13.41 13.17L9.41 9.17L2 16.59L3.41 18L9.41 12L13.41 16L19.71 9.71L22 12V6H16Z" fill="currentColor"/>
-                  </svg>
-                </div>
                 <div className="value-text">성장</div>
               </div>
             </div>
